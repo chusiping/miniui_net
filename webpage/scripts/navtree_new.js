0214d197495295b3f2f989cab7611754e85fab87
chusiping <chusiping@163.com>
2018-01-17 14:35:14 +0800

    2 增加自定义的navtree_temp.html 文件

---
+++ webpage/scripts/navtree_new.js
@@ -0,0 +1,55 @@
+﻿ mini.parse();
+
+        var tree = mini.get("leftTree");
+
+        function showTab(node) {
+            var tabs = mini.get("mainTabs");
+
+            var id = "tab$" + node.id;
+            var tab = tabs.getTab(id);
+            if (!tab) {
+                tab = {};
+                tab._nodeid = node.id;
+                tab.name = id;
+                tab.title = node.text;
+                tab.showCloseButton = true;
+
+                //这里拼接了url，实际项目，应该从后台直接获得完整的url地址
+                tab.url = "/docs/api/" + node.id + ".html";
+                //tab.url = mini_JSPath + "/docs/api/" + node.id + ".html";
+
+                tabs.addTab(tab);
+            }
+            tabs.activeTab(tab);
+        }
+
+        function onNodeSelect(e) {
+        
+            var node = e.node;
+            var isLeaf = e.isLeaf;
+
+            if (isLeaf) {
+                showTab(node);
+            }
+        }
+
+        function onClick(e) {
+            var text = this.getText();
+            alert(text);
+        }
+        function onQuickClick(e) {
+            tree.expandPath("datagrid");
+            tree.selectNode("datagrid");
+        }
+
+        function onTabsActiveChanged(e) {
+            var tabs = e.sender;
+            var tab = tabs.getActiveTab();
+            if (tab && tab._nodeid) {
+
+                var node = tree.getNode(tab._nodeid);
+                if (node && !tree.isSelectedNode(node)) {
+                    tree.selectNode(node);
+                }
+            }
+        }
